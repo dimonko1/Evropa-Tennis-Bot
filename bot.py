5e75adad6aa3d99d51c1ec8b7344ecb08199c3ed
Dmitry Konev <109601367+dimonko1@users.noreply.github.com>
2025-02-27 22:49:43 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -108,4 +108,4 @@ async def process_callback(callback_query: types.CallbackQuery):
 
 # Запуск бота
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)_date
+    executor.start_polling(dp, skip_updates=True)
