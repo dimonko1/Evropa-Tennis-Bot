64bc319cb2bb74cd178e30bdfe02ea892693562b
Dmitry Konev <109601367+dimonko1@users.noreply.github.com>
2025-02-27 23:31:42 +0300

    config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "8092903063:AAEwoCKEXnjSSG6CRn6AqhaIumlnz5SvZzk"
+TOKEN = "8092903063:AAHUlT1CbX1ochMUoZdgVv_T0jzzkTSYyiE"
 DB_URL = "postgresql://evropa_tennis_bot_user:diqEKRwZ4LPfWOWvRijYkR7LbCUXS7xN@dpg-cv0b601u0jms73fbpr9g-a/evropa_tennis_bot"
